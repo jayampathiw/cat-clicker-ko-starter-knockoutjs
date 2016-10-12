a69830ad864286e635610eaa03129e1e4438ed8e
Jayampathi Wijesena <jayampathi@ebuilder.local>
2016-10-12 06:23:35 +0530

    Introduce a new Cat Object

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
-function AppViewModel() {
+var Cat = function(){
    this.clickCount = ko.observable(0);
    this.name = ko.observable("Tobby");
    
    this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
    this.imgAttribute = ko.observable('www.google.com');
 
+   this.level = ko.computed(function(){
+         if(this.clickCount() < 10){
+            return "New Born";
+         } else if(this.clickCount() < 20){
+            return "Infent";
+         } else if(this.clickCount() < 30) {
+            return "teen"
+         } else {
+            return "well grown";
+         }
+   }, this);
+
+   this.nickNames = ko.observableArray([
+        { name: 'Bert' },
+        { name: 'Charles' },
+        { name: 'Denise' }
+    ]);
+}
+
+
+
+function AppViewModel() {
+   
+   this.currentCat = ko.observable(new Cat());
    this.incrementCounter = function(){
-   		this.clickCount(this.clickCount() + 1);
+   		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
    }
 
-   this.level = ko.computed(function(){
-   		if(this.clickCount() > 10){
-   			return "Infent";
-   		} else if(this.clickCount() < 10 && this.clickCount() > 20) {
-   			return "teen"
-   		} else {
-   			return "New Born";
-   		}
-   }, this);
+   
 }
 
 // Activates knockout.js
