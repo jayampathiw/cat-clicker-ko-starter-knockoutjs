e68d73a3e5e41a2b7fe7169b7332779f05e642c2
Jayampathi Wijesena <jayampathi@ebuilder.local>
2016-10-12 06:59:34 +0530

    passing parameters

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var Cat = function(){
-   this.clickCount = ko.observable(0);
-   this.name = ko.observable("Tobby");
+var Cat = function(data){
+   this.clickCount = ko.observable(data.clickCount);
+   this.name = ko.observable(data.name);
    
-   this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
-   this.imgAttribute = ko.observable('www.google.com');
+   this.imgSrc = ko.observable(data.imgSrc);
+   this.imgAttribute = ko.observable(data.imgAttribute);
 
    this.level = ko.computed(function(){
          if(this.clickCount() < 10){
@@ -17,20 +17,26 @@ var Cat = function(){
          }
    }, this);
 
-   this.nickNames = ko.observableArray([
-        { name: 'Bert' },
-        { name: 'Charles' },
-        { name: 'Denise' }
-    ]);
+   this.nickNames = ko.observableArray(data.nickNames);
 }
 
 
 
 function AppViewModel() {
-   
-   this.currentCat = ko.observable(new Cat());
-   this.incrementCounter = function(){
-   		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+   self = this;
+   self.currentCat = ko.observable(new Cat({
+      clickCount : 0,
+      name : "Tobby",
+      imgSrc : 'img/1413379559_412a540d29_z.jpg',
+      imgAttribute : 'www.google.com',
+      nickNames : [
+        { name: 'Bert' },
+        { name: 'Charles' },
+        { name: 'Denise' }
+    ]
+   }));
+   self.incrementCounter = function(){
+   		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
    }
 
    
